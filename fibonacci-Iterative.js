f95d268196eb950ee82f69282f248a3437b44b47
ttahb <bhattvijay69@hotmail.com>
2021-09-25 11:24:12 +0530

    added fibonacci iterative and recursive approach

---
+++ fibonacci-Iterative.js
@@ -0,0 +1,25 @@
+//Given a number n, return the index value of that number in fibonacci series
+//e.g. if n = 2, as per the fibonaaci  series 0,1,1,2,3,...   it should return index value at 2 that is return 1. If n = 3, return 2
+function fibonacciIterative(n){
+    if(n === 0 || n === 1){
+        return n;
+    } else {
+        let n1 =0;
+        let n2 = 1;
+        let temp = 0;
+
+        for(let i = 1; i < n; i++){
+            temp = n1;
+            n1 = n2;
+            n2 = temp+n2;
+        }
+
+        return n2;
+    }
+}
+
+console.log(fibonacciIterative(0));
+console.log(fibonacciIterative(2));
+console.log(fibonacciIterative(3));
+console.log(fibonacciIterative(6));
+console.log(fibonacciIterative(8));
---
+++ fibonacci-recursive.js
@@ -0,0 +1,17 @@
+//a recursive approach to fibonacci series.
+function fibonacciRecursive(n){
+    if(n===0 || n === 1){
+        return n;
+    } else {
+        return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+    }
+}
+
+console.log(fibonacciRecursive(0));
+console.log(fibonacciRecursive(1));
+console.log(fibonacciRecursive(2));
+console.log(fibonacciRecursive(3));
+console.log(fibonacciRecursive(4));
+console.log(fibonacciRecursive(5));
+console.log(fibonacciRecursive(6));
+console.log(fibonacciRecursive(7));
