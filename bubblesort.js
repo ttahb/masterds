0a617376ae518ca9cf26daf1a55713737923e59f
ttahb <bhattvijay69@hotmail.com>
2021-09-28 17:39:26 +0530

    updated bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,11 +1,12 @@
 const numbers = [6,5,3,1,8,7,2,4];
 
 function bubbleSort(array){
-   let flag = false;
-   
+    let len = array.length;
+    let flag = false;
+
    do {
     flag = false;
-    for(let i=0; i<array.length-1;i++){
+    for(let i=0; i<len-1;i++){
         let temp = array[i];
         if(array[i]>array[i+1]){
             array[i] = array[i+1];
