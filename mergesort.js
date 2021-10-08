365701674e41e7df66c3e664bee1e09391b9ed10
ttahb <bhattvijay69@hotmail.com>
2021-10-08 11:53:18 +0530

    added mergesort code

---
+++ mergesort.js
@@ -0,0 +1,42 @@
+const numbers = [6,5,3,1,0,8,7,2,9,4];
+
+function mergeSort(array){
+    if(array.length === 1){
+        return array;
+    }
+
+    //Split array into right and left
+    let mid = array.length/2;
+    const left = array.slice(0,mid);
+    const right = array.slice(mid);
+
+    return merge( mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right){
+    
+    const newArr = [];
+    let i = 0,j = 0;
+   
+    while(i<left.length && j<right.length){
+
+        if(left[i]<right[j]){
+            newArr.push(left[i]);
+            i++;
+        } else {
+            newArr.push(right[j]);
+            j++;
+        }
+    }
+
+    if(i == left.length){
+        newArr.push(...right.slice(j));
+    } else {
+        newArr.push(...left.slice(i));
+    }
+
+    return newArr;
+}
+
+const answer = mergeSort(numbers);
+console.log(answer);
